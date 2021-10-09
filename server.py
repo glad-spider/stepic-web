9f917c2c332e89d7ddff2176a59dcb26299ff264
KAVALEUSKI TSIMAFEI <frstnm.lstnm.1444@mail.ru>
2021-10-09 17:05:15 +0300

    first commit add task 1.6

---
+++ server.py
@@ -0,0 +1,12 @@
+import socket
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind(("0.0.0.0",2222))
+s.listen(10)
+while True:
+    conn, addr = s.accept()
+    data = conn.recv(1024)
+    if not data or data == "close": 
+        conn.close()
+        break
+    conn.send(data)
+    conn.close()
